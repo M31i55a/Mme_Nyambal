268e329a34cb3d04489f060617d08477c47fc23c
M31i55a <melysgioda34@gmail.com>
2025-01-18 10:19:04 +0100

    second version pending

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { gsap } from 'https://cdn.skypack.dev/gsap';
+gsap.registerPlugin(CustomEase);
+
+let loading = document.querySelector(".loading_page");
+document.addEventListener("DOMContentLoaded", (event) => {
+    let video = document.getElementById("loader-video");
+    video.addEventListener("ended", function () {
+      loadContent();
+    });
+  });
+
+  function loadContent() {
+    let video = document.getElementById("loader-video");
+    video.style.opacity = 0; // Fade out the video
+    setTimeout(function () {
+      video.style.display = "none";
+    }, 1000); // Wait for the fade-out to complete
+    let content = document.getElementById("welcome_page");
+    content.style.opacity = 1; // Fade in the content gradually
+    loading.style.display = "none";
+  }
+
+ loading.addEventListener('wheel', (event) => {
+    event.preventDefault(); 
+  });
 
 
-//Counter&Loader
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(CustomEase);
+  document.addEventListener("DOMContentLoaded", () => {
+    
     CustomEase.create("hop", "M0,0 C0.126,0.382 0.2,0.64 0.3,0.64 0.4,0.64 0.4,0 0.6,0 0.8,0 0.8,1 1,1");
 
     function splitTextIntoSpans(selector){
@@ -17,157 +39,234 @@ document.addEventListener("DOMContentLoaded", () => {
             element.innerHTML = spans.join("");
         });
     }
-    splitTextIntoSpans(".welcome_container h1");
+    splitTextIntoSpans(".content_body h1");
 
-    function animateCounter(){
-        const counterElement = document.querySelector(".counter p");
-        let currentValue = 0;
-        let updateInterval = 200;
-        const maxDuration = 2000;
-        const endValue = 100;
-        const startTime = Date.now();
-
-        function updateCounter(){
-            const elapsedTime = Date.now() - startTime;
-            if(elapsedTime < maxDuration){
-                currentValue = Math.min(currentValue + Math.floor(Math.random() * 30) + 5, endValue);
-
-                counterElement.textContent = currentValue;
-                setTimeout(updateCounter, updateInterval);
-            }
-            else{
-                counterElement.textContent = currentValue;
-                setTimeout(() => {
-                    gsap.to(counterElement, {y: -20, duration: 1, ease: "power3.out", onStart:() => {
-                        revealLandingPage();
-                    }});
-                }, -500)
-            }
-        }
-
-        updateCounter();
-    }
-
-    gsap.to(".counter p", {
+    gsap.to(".content_body h1 span", {
         y: 0,
-        duration: 1,
-        ease: "power3.out",
-        delay: 1,
-        onComplete: animateCounter,
-    })
-
-    function revealLandingPage(){
-        gsap.to(".hero", {
-            clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
-            duration: 2,
-            ease: "hop",
-            onStart: () => {
-                gsap.to(".hero", {
-                    transform: "translate(-50%, -50%) scale(1)",
-                    duration: 2.25,
-                    ease: "power3.inOut",
-                    delay: 0.25,
-                })
-
-                gsap.to(".overlay", {
-                    clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-                    duration: 2,
-                    delay: 0.5,
-                    ease: "hop",
-                })
-
-                gsap.to(".hero-img img", {
-                    transform: "scale(1)",
-                    duration: 2.25,
-                    ease: "power3.inOut",
-                    delay: 0.25,
-                })
-
-                gsap.to(".header h1 span", {
-                    y: 0,
-                    stagger: 0.1,
-                    duration: 2,
-                    ease: "power4.inOut",
-                    delay: 0.75,
-                })
-            }
-        })
-    }
+        stagger: 0.1,
+        duration: 2,
+        ease: "power4.inOut",
+        delay: 6,
+    })   
 })
 
 
-//svg animation
-let path = document.querySelector("path");
-let pathLength = path.getTotalLength();
-path.style.strokeDasharray = pathLength + " " + pathLength;
-path.style.strokeDashoffset = pathLength;
-
-const welcomeElement = document.querySelector('.welcome');
-
-window.addEventListener("scroll", function (e) {
-    const welcomeTop = welcomeElement.getBoundingClientRect().top + (welcomeElement.offsetHeight / 2);
-    if (welcomeTop <= window.innerHeight / 2) {
-        let scrollPercentage =
-        (document.documentElement.scrollTop + document.body.scrollTop) /
-        (document.documentElement.scrollHeight -
-        document.documentElement.clientHeight);
-        let drawLength = pathLength * scrollPercentage;
-    path.style.strokeDashoffset = pathLength - drawLength;
-    }
+let text = document.getElementById("text");
+let koleteImg = document.querySelector(".content_body .image_container")
+// let p = document.querySelector("p");
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  text.style.bottom = value * -0.6 + "px";
+  text.style.opacity = 1 - value * 0.001;
+  koleteImg.style.bottom = value * -0.45 + "px";
+//   p.style.bottom = value * 0.2 + "px";
 });
 
-// Fade out SVG when .home hits the top
-const homeElement = document.querySelector('.home');
-const svgElement = document.querySelector('svg');
+//about
+gsap.timeline({
+  scrollTrigger: {
+    trigger: "#about",
+    start: "3.5% top",
+    end: () => innerHeight * 3.65,
+    scrub: true,
+    pin: "#about .description",
+    anticipatePin: 1
+  }
+})
+.set("#about img", {width: "100%"})
+.set("#about .description_block:nth-child(1)", {width: 0, autoAlpha: 0})
+.set("#about .block_container:nth-child(1) .image_block", {autoAlpha: 0, width: "100%", duration: 0.4})
+.to("#about .block_container:nth-child(1) .image_block", {duration: 0.4, autoAlpha: 1})
+.to("#about .description_block:nth-child(1)", {width: "50%"})
+.to("#about .block_container:nth-child(1) .image_block", {duration: 0.4, width: "50%"})
+.to("#about .description_block:nth-child(1)", {y: "-10%" ,autoAlpha: 1, duration: 1.2});
 
-window.addEventListener('scroll', () => {
-  const homeTop = homeElement.getBoundingClientRect().top;
-  if (homeTop <= 0) {
-    const opacity = Math.max(0, 1 - (window.scrollY / window.innerHeight));
-    svgElement.style.opacity = opacity;
-  } else {
-    svgElement.style.opacity = 1;
+  // Pin again and set opacity to 0
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: "#about .description",
+      start: "top top",
+      end: () => innerHeight * 3.62,
+      pin: true,
+      scrub: true,
+      anticipatePin: 1
+    }
+  })
+  .to("#about .block_container:nth-child(1)", {autoAlpha: 0, duration: 0.5})
+  .to("#about .block_container:nth-child(2)", { y: "-17%" ,autoAlpha: 1, duration: 1});
+
+
+
+
+//lifestyle scroll
+gsap.registerPlugin(ScrollTrigger);
+let proxy = { skew: 0 },
+    skewSetter = gsap.quickSetter("#lifestyle img", "skewY", "deg"), // fast
+    clamp = gsap.utils.clamp(-5, 5); // don't let the skew go beyond 20 degrees. 
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / -100);
+    // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {skew: 0, duration: 0.5, ease: "power2", overwrite: true, onUpdate: () => skewSetter(proxy.skew)});
+    }
   }
 });
 
+// make the right edge "stick" to the scroll bar. force3D: true improves performance
+gsap.set("#lifestyle img", {transformOrigin: "top center", force3D: true});
 
 
-//Welcome animation
-let text = document.getElementById("text");
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  text.style.bottom = value * -0.7 + "px";
-});
+    let getRatio = el => window.innerHeight / (window.innerHeight + el.offsetHeight/2.5);
 
-
-const stickySections = [...document.querySelectorAll('.lifestyle .description, .inspiration .description')];
-const lifestyle = document.querySelector('.lifestyle');
-
-window.addEventListener('scroll', (e) => {
-    const lifestyleTop = lifestyle.getBoundingClientRect().top;
-    if (lifestyleTop <= 0) {
-        for (let i = 0; i < stickySections.length; i++) {
-            transform(stickySections[i]);
+    gsap.utils.toArray("#lifestyle .description_blocks .image_blocks img").forEach((img, i) => {
+      // use function-based values in order to keep things responsive
+      gsap.to(img,  {
+        y: () => window.innerHeight * (1 - getRatio(img)),
+        ease: "none",
+        scrollTrigger: {
+          trigger: img,
+          start: () => i ? "top bottom" : "top top", 
+          end: "bottom top",
+          scrub: true,
+          invalidateOnRefresh: true // to make it responsive
         }
-    }
-});
+      });
+    });
 
-function transform(section) {
-    const offsetTop = section.parentElement.offsetTop;
-    const scrollSection = section.querySelector('.scroll_section');
-    let percentage = (window.scrollY - offsetTop) / (window.innerHeight) * 100;
-    scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
+    // make the right edge "stick" to the scroll bar. force3D: true improves performance
+    gsap.set("#lifestyle .description_blocks", {transformOrigin: "right center", force3D: true});
+
+
+
+
+// Stars Animation
+
+// Get canvas and context
+const canvas0 = document.getElementById('stars');
+const canvas_ctx = canvas0.getContext('2d');
+
+// Set canvas dimensions
+canvas0.width = window.innerWidth;
+canvas0.height = window.innerHeight + window.innerWidth;
+
+// Mouse coordinates
+const mouse0 = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+let myMouse = {
+  x: undefined,
+  y: undefined
+};
+
+// Particle colors
+const colors = ['#2185c5', '#7ecefd', '#fff6e5', '#ffff00'];
+
+// Mouse down flag
+let mousedown = false;
+window.addEventListener('mousedown', () => {
+  mousedown = true;
+});
+window.addEventListener('mouseup', () => {
+  mousedown = false;
+});
+window.addEventListener('mousemove', (e) => {
+  myMouse.x = e.clientX;
+  myMouse.y = e.clientY;
+})
+
+// Particle class
+class Particles {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw1() {
+    canvas_ctx.beginPath();
+    canvas_ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    canvas_ctx.fillStyle = this.color;
+    canvas_ctx.shadowColor = this.color;
+    canvas_ctx.shadowBlur = 20;
+    canvas_ctx.fill();
+    canvas_ctx.closePath();
+    
+  }
+
+  update() {
+    // this.draw1();
+  }
 }
+
+// Initialize particles
+let particles = [];
+
+function init1() {
+  particles = [];
+
+  for (let i = 0; i < 1000; i++) {
+    const canvasWidth = canvas0.width * 2;
+    const canvasHeight = canvas0.height * 2;
+    const x = Math.random() * canvasWidth - canvasWidth / 2;
+    const y = Math.random() * canvasHeight - canvasHeight / 2;
+    const radius = Math.random() * 1.3;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    particles.push(new Particles(x, y, radius, color));
+  }
+}
+
+// Animation function
+let radians = 0;
+let alpha = 1;
+
+function animate1() {
+  requestAnimationFrame(animate1);
+  canvas_ctx.fillStyle = `rgba(10, 10, 10, ${alpha})`;
+  canvas_ctx.fillRect(0, 0, canvas0.width, canvas0.height);
+
+  canvas_ctx.save();
+  canvas_ctx.translate(canvas0.width / 2, canvas0.height / 2);
+  canvas_ctx.rotate(radians);
+
+  particles.forEach((particle) => {
+    //newly added
+    particle.draw1();
+    particle.update();
+  })
+
+  canvas_ctx.restore();
+
+  radians += 0.001;
+
+  if (mousedown && alpha >= 0.1) {
+    alpha -= 0.05;
+    radians += 0.015;
+  } else if (!mousedown && alpha < 1) {
+    alpha += 0.01;
+  }
+  
+}
+
+// Initialize and start animation
+init1();
+animate1();
 
 
 
 // 3D rendering
 
 const camera = new THREE.PerspectiveCamera(
-    10,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  10,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 camera.position.z = 21;
 
@@ -176,18 +275,18 @@ let bee;
 let mixer;
 const loader = new GLTFLoader();
 loader.load('/assets/microphone.glb',
-    function (gltf) {
-        bee = gltf.scene;
-        scene.add(bee);
+  function (gltf) {
+      bee = gltf.scene;
+      scene.add(bee);
 
-        mixer = new THREE.AnimationMixer(bee);
-        mixer.clipAction(gltf.animations[0]).play();
-        modelMove();
-    },
-    function (xhr) {},
-    function (error) {
-        console.error('An error happened', error);
-    }
+      mixer = new THREE.AnimationMixer(bee);
+      mixer.clipAction(gltf.animations[0]).play();
+      modelMove();
+  },
+  function (xhr) {},
+  function (error) {
+      console.error('An error happened', error);
+  }
 );
 
 // Add lighting
@@ -211,18 +310,45 @@ document.getElementById('container3D').appendChild(renderer.domElement);
 // Animation loop
 
 const animate = () => {
-    scene.rotation.y += 0.005;
+  scene.rotation.y += 0.005;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
 window.addEventListener('scroll', () => {
-        if (bee) {
-        const scrollY = window.scrollY;
-        scene.rotation.y = scrollY * 0.005;
-        light.position.set(0, scrollY, scrollY).normalize();
-        pointLight.position.set(105, scrollY, scrollY);
-    }
+      if (bee) {
+      const scrollY = window.scrollY;
+      scene.rotation.y = scrollY * 0.005;
+      light.position.set(0, scrollY, scrollY).normalize();
+      pointLight.position.set(105, scrollY, scrollY);
+  }
 })
+
+//audio list
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+let audio = document.querySelectorAll('#talk_with_me .menu li');
+let play_content = document.querySelector('.play_content');
+let h1 = document.querySelector('#talk_with_me h1');
+let content = document.querySelector(".content");
+let imgBx = document.querySelector(".image_container");
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  content.classList.toggle('display_playing');
+  h1.classList.toggle('display');
+  play_content.classList.toggle('display_playing');
+});
+
+audio.forEach((audio) => {
+  audio.addEventListener('click', () => {
+    const currentlyPlaying = document.querySelector('.playing');
+    if (currentlyPlaying) {
+      currentlyPlaying.classList.remove('playing');
+    }
+    audio.classList.toggle('playing');
+    
+  });
+});
